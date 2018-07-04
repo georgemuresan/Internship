d618deff45cdff179abc93a1c2f847c32048bd59
George Muresan <georgecatalinmuresan@gmail.com>
2018-07-04 14:46:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/admin/sleepbetter/FirstPage.java
@@ -0,0 +1,35 @@
+package com.example.admin.sleepbetter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+
+        Button button = (Button) findViewById(R.id.startButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                goToSecondActivity();
+
+            }
+
+        });
+    }
+    private void goToSecondActivity() {
+
+        Intent intent = new Intent(this, SecondPage.class);
+
+        startActivity(intent);
+
+    }
+}
