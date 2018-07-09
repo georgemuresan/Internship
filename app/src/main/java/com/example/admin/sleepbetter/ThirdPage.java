a4903d6d9b5dd6ede48d403660cae34bf4cf32ef
George Muresan <georgecatalinmuresan@gmail.com>
2018-07-09 19:14:12 +0100

    changes

--- app/src/main/java/com/example/admin/sleepbetter/ThirdPage.java
+++ app/src/main/java/com/example/admin/sleepbetter/ThirdPage.java
@@ -1,7 +1,10 @@
 package com.example.admin.sleepbetter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdPage extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class ThirdPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_page);
+
+        Button button = (Button) findViewById(R.id.startButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                goToMainMenu();
+
+            }
+
+        });
+    }
+    private void goToMainMenu() {
+
+        Intent intent = new Intent(this, MainMenu.class);
+
+        startActivity(intent);
+
     }
 }
