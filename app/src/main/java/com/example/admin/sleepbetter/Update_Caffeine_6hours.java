29dd3558c9a8300a7e0a04d12006566b3e44ac83
George Muresan <georgecatalinmuresan@gmail.com>
2018-07-19 16:21:57 +0100

    DaoAccess, MovieDatabase, Movies, Report

--- app/src/main/java/com/example/admin/sleepbetter/Update_Caffeine_6hours.java
+++ app/src/main/java/com/example/admin/sleepbetter/Update_Caffeine_6hours.java
@@ -2,18 +2,23 @@ package com.example.admin.sleepbetter;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.arch.persistence.room.Room;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TimePicker;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Update_Caffeine_6hours extends Fragment {
     private SeekBarWithIntervals dayReviewBar = null;
+    private static final String DATABASE_NAME = "user_db";
+    private UserDatabase userDatabase;
+
     View updateView;
     @Nullable
     @Override
@@ -39,6 +44,31 @@ public class Update_Caffeine_6hours extends Fragment {
     }
 
     public void goToQuestionnaire(){
+
+       // final TimePicker tpLastdrink = (TimePicker) updateView.findViewById(R.id.lastDrink);
+      //  String lastDrink = tpLastdrink.getCurrentHour() + ":" + tpLastdrink.getCurrentMinute();
+
+      //  final TimePicker tpWhenSleep = (TimePicker) updateView.findViewById(R.id.whenSleep);
+    //    String whenSleep = tpWhenSleep.getCurrentHour() + ":" + tpWhenSleep.getCurrentMinute();
+//
+    //    final int dayReview = dayReviewBar.getProgress();
+
+    /*    new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                userDatabase = Room.databaseBuilder(getActivity().getApplicationContext(), UserDatabase.class, DATABASE_NAME).fallbackToDestructiveMigration().build();
+                UserExperiment user = new UserExperiment();
+
+
+               // userDatabase.daoAccess().insertSingleUserQuestionnaire(user);
+
+               // Report rep = new Report(userDatabase, getActivity().getApplicationContext());
+              //  rep.save();
+            }
+        }).start();
+
+*/
         FragmentManager fragmentManager = getFragmentManager();
 
         fragmentManager.beginTransaction().replace(R.id.content_frame, new Questionnaire()).commit();
