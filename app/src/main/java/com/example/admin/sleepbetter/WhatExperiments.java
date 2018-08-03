e5a2fcce902254c3f58fd96be28029e1d2d91979
RdVoitz <rdv1u16@soton.ac.uk>
2018-08-03 14:33:39 +0100

    last bitmojis

--- app/src/main/java/com/example/admin/sleepbetter/WhatExperiments.java
+++ app/src/main/java/com/example/admin/sleepbetter/WhatExperiments.java
@@ -1,10 +1,12 @@
 package com.example.admin.sleepbetter;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class WhatExperiments extends AppCompatActivity {
 
@@ -12,6 +14,18 @@ public class WhatExperiments extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.what_experiments);
+
+        SharedPreferences preferences = getSharedPreferences("bmwalking", MODE_PRIVATE);
+        ImageView imageView = findViewById(R.id.imageView23);
+        imageView.setImageResource(preferences.getInt("slectedbitmoji", 0));
+
+        preferences = getSharedPreferences("bmbad", MODE_PRIVATE);
+        imageView = findViewById(R.id.imageView24);
+        imageView.setImageResource(preferences.getInt("slectedbitmoji", 0));
+
+        preferences = getSharedPreferences("bmhappy", MODE_PRIVATE);
+        imageView = findViewById(R.id.imageView25);
+        imageView.setImageResource(preferences.getInt("slectedbitmoji", 0));
 
         Button button = (Button) findViewById(R.id.back);
 
