400d73f90ea9acc48252fbcbe619ce4d5d0339dc
George Muresan <georgecatalinmuresan@gmail.com>
2018-08-13 16:24:48 +0100

    Merge branches 'master' and 'withoutbit' of https://github.com/georgemuresan/Internship into withoutbit
    # Conflicts:
    #	app/src/main/java/com/example/admin/sleepbetter/FirstPage.java
    #	app/src/main/java/com/example/admin/sleepbetter/SecondPage.java

--- app/src/main/java/com/example/admin/sleepbetter/Data.java
+++ app/src/main/java/com/example/admin/sleepbetter/Data.java
@@ -105,8 +105,8 @@ public class Data extends Fragment implements AdapterView.OnItemSelectedListener
                 graph.setTitleColor(blackValue);
                 // graph.setTitleTextSize(13);
                 graph.getViewport().setMinX(0);
-                graph.getViewport().setMaxX(20);
-                graph.getViewport().setMaxXAxisSize(20);
+                graph.getViewport().setMaxX(15);
+                graph.getViewport().setMaxXAxisSize(15);
                 graph.getViewport().setMinY(1);
                 graph.getViewport().setMaxY(5);
 
@@ -396,8 +396,8 @@ public class Data extends Fragment implements AdapterView.OnItemSelectedListener
                 graph.setTitleColor(blackValue);
                 // graph.setTitleTextSize(13);
                 graph.getViewport().setMinX(0);
-                graph.getViewport().setMaxX(20);
-                graph.getViewport().setMaxXAxisSize(20);
+                graph.getViewport().setMaxX(15);
+                graph.getViewport().setMaxXAxisSize(15);
                 graph.getViewport().setMinY(1);
                 graph.getViewport().setMaxY(5);
 
